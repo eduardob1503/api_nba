d8c7e0d7429d6031304a53f7828fd679ef8fefd3
Eduardo Barcelos VIana <eticleo@yahoo.com.br>
2026-04-23 14:10:23 -0300

    render+local

--- database.py
+++ database.py
@@ -1,15 +1,16 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
-from config import DATABASE_URL
+import config
 load_dotenv()
 
 def conectar():
     ENV = os.getenv("ENV")
-
+    DATABASE_URL = os.getenv("DATABASE_URL")
     if ENV == "production":
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
         return conn
     else:
+        DATABASE_URL = config.DATABASE_URL
         conn = psycopg2.connect(**DATABASE_URL)
         return conn
